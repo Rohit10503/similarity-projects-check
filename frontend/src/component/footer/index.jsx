2cd7848c1086891f3d15902d1570a891cd5de6de
Rohit10503 <117336016+Rohit10503@users.noreply.github.com>
2025-02-15 20:34:18 +0530

    Admin

--- frontend/src/component/footer/index.jsx
+++ frontend/src/component/footer/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./footer.css"
+import { useNavigate } from "react-router-dom";
 const Footer=()=>{
+    const navigate=useNavigate()
+    const goToAdmin=()=>{
+        navigate("/adashboard")
+    }
     return<>
     <div className="footer-main">
-        <h1 className="title   ">Developed and Maintained by Group 4</h1>
+        <h1 onClick={goToAdmin()} className="title   ">Developed and Maintained by Group 4 </h1>
+        
     </div>
     </> 
 }
